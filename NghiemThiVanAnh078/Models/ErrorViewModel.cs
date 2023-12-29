145ecda8436f8f5510547805f55e52625c10426f
nghiemthivananh <nghiemthivananh13112002@gmail.com>
2023-12-29 12:47:38 +0700

    first commit

---
+++ NghiemThiVanAnh078/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NghiemThiVanAnh078.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
